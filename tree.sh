1e5a3efbfa546d7d24e416fdab7f91224754e039
Tait-A <s2051131@ed.ac.uk>
2024-06-22 12:24:09 +0200

    trees

--- tree.sh
+++ tree.sh
@@ -2,6 +2,8 @@
 
 # This script will print the directory tree of the current directory
 
+
+### ARGUMENT PROCESSING ###
 levels=2
 
 while getopts "l:" opt; do
@@ -22,32 +24,37 @@ else
     root_dir="."
 fi
 
+### RECURSIVE FUNCTION TO PRINT TREE ###
+
 print_tree() {
     local directory=$1
     local cur_level=$2
-    local prev_prefix=$3
+    local cur_prefix=$3
 
     if [ $cur_level -gt $levels ]; then
         return
     fi
 
     local prefix=$(printf "%*s" $((((cur_level - 1)) * 4)) "")
+    local files = ($(ls -a "$directory"))
 
-    for entry in "$directory"/*; do
-        if #is last in $directory then
-            local dir_prefix="${prefix}└──"
+    for ((i=0; i<${#files[@]}; i++)); do
+        local entry = $files[i]
+        if $i == ${#files[@]} - 1; then
+            dir_prefix="${prefix}└──"
+            local next_prefix="${cur_prefix}    "
         else
-            local dir_prefix="${prefix}├──"
+            dir_prefix="${prefix}├──"
+            local next_prefix="${cur_prefix}│   "
         fi
-        local dir_prefix =
         if [ -d "$entry" ]; then
-            echo "${prefix}├──$(basename "$entry")"
-            print_tree "$entry" $((cur_level + 1))
+            echo "${dir_prefix}$(basename "$entry")"
+            print_tree "$entry" $((cur_level + 1)) "$next_prefix"
         else
-            echo "${prefix}├──$(basename "$entry")"
+            echo "${prefix}$(basename "$entry")"
         fi
     done
 }
 
 echo "$root_dir"
-print_tree "$root_dir" 1
+print_tree "$root_dir" 1 ""
